297baaf8768385993457993d1e593bac5f421c41
yaminvolazi <yamin.volazi@gmail.com>
2017-10-03 10:17:37 +0100

    commit 6

--- resources/views/Stores.blade.php
+++ resources/views/Stores.blade.php
@@ -1,17 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CITYSHOP Stores</title>
-</head>
-<body>
-    <h1>CITYSHOP / Stores</h1>
+@extends('layouts.master')
+
+@section('content')
+
+<h1>CITYSHOP / Stores</h1>
     @foreach($stores as $store)
         <h3>{{$store->name}}</h3>
         <hr>
     @endforeach
-    
-</body>
-</html>
+
+@endsection
